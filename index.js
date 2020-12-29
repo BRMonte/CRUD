092c6d5aa2f3510a5245eb4db53bbdb78f2ca3e5
Bruno Montebello <brunormontebello@hotmail.com>
2020-12-29 13:31:25 +0000

    Declare global variables

---
+++ index.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', start);
+
+//declarando variaveis globais
+var globalNames = ["um", "dois", "tres", "quatro", "cinco"];
+var nomes = document.querySelector('#nomes');
+var ul = document.createElement('ul');
+var input = document.getElementById('Input')
+var form = document.getElementById('Formulario');
+var isEditing = false;
+var Posiçao;
